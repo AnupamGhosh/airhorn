b305632985b4d1cd06b8f261bef267858455f91e
Anupam Ghosh <i_anupam_ghosh@yahoo.co.in>
2017-10-14 18:42:36 +0530

    bug fixed

--- sw.js
+++ sw.js
@@ -20,6 +20,6 @@ self.addEventListener('fetch', function(event) {
     caches.match(event.request)
       .then(function(response) {
         return response || fetch(event.request);
-      });
-  )
+      })
+  );
 });
